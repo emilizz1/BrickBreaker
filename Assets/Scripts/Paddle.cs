2a6768a2cefcd080f9e3cdb6a4ad4949d2a53f45
Emilijus <slibinas2012@gmail.com>
2019-02-08 23:34:25 +0200

    Added another level and fixed some bugs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -31,7 +31,7 @@ public class Paddle : MonoBehaviour {
 	void MousePlay()
 	{
 		Vector3 paddlePos = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
-		float mousePosInBlocks = Mathf.Clamp( Input.mousePosition.x / Screen.width * 16, 0.82f, 15.18f);
+		float mousePosInBlocks = Mathf.Clamp( Input.mousePosition.x / Screen.width * 16, 0.82f * transform.localScale.x, 16f - 0.82f * transform.localScale.x); 
 		paddlePos.x = mousePosInBlocks;
 		this.transform.position = paddlePos;
 	}
